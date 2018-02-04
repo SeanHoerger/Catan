753e3252e4a5f493f0778ce31c241e34696ae974
aroeseler <35982361+aroeseler@users.noreply.github.com>
2018-02-03 21:56:43 -0800

    Add files via upload

---
+++ SettlersOfCatan/src/Catan/Program.java
@@ -0,0 +1,12 @@
+package Catan;
+import java.util.Random;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random rand = new Random();
+		System.out.println(rand);
+	}
+
+}
